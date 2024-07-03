f7eff013fcda54cfa57f7d3938cddd9d345de2f2
Madpeacher <rainbownatural@yandex.ru>
2024-07-03 14:49:24 +0300

    Update aa.go
    abrocadabra

--- aa.go
+++ aa.go
@@ -22,7 +22,7 @@ var artor = []struct {
 	{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}, // для конвертации арабских чисел в римские в программе калькулятора.
 }
 
-func rotoint(s string) (int, error) {
+func rotoint(s string) int {
 	n := len(s)          // определяем длину строки s, чтобы знать, сколько символов нужно обработать.
 	res := 0             // результат конвертации римских чисел в арабские.
 	for i := 0; i < n; { //  цикл, который будет перебирать символы в строке s.
@@ -38,10 +38,10 @@ func rotoint(s string) (int, error) {
 			res += value
 			i++
 		} else {
-			return 0, fmt.Errorf("Panic") // неверное значение римской цифры
+			panic("Panic: Invalid Roman numeral") // неверное значение римской цифры
 		}
 	}
-	return res, nil // В конце функции возвращаем res и nil или ошибку в случае неверного ввода римской цифры.
+	return res
 }
 
 func intor(num int) string { // преобразование арабских чисел в их римское представление, используя массив artor для определения соответствующих значений и символов.
@@ -59,57 +59,57 @@ func intor(num int) string { // преобразование арабских ч
 	return res.String()
 }
 
-func calculate(a, b int, op string) (int, error) {
+func calculate(a, b int, op string) int {
 	switch op {
 	case "+":
-		return a + b, nil
+		return a + b
 	case "-":
-		return a - b, nil
+		return a - b
 	case "*":
-		return a * b, nil
+		return a * b
 	case "/":
 		if b == 0 {
-			return 0, fmt.Errorf("Panic") // делить на 0 нельзя
+			panic("Panic: Division by zero") // делить на 0 нельзя
 		}
-		return a / b, nil
+		return a / b
 	default:
-		return 0, fmt.Errorf("Panic") // неверный оператор
+		panic("Panic: Invalid operator") // неверный оператор
 	}
 }
 
-func proinp(input string) (string, error) {
+func proinp(input string) string {
 	p := strings.Fields(input) // разбивается на отдельные слова
 	if len(p) != 3 {           // Проверяется, что строка разбита на ровно три части. Если это условие не выполняется, возвращается ошибка
-		return "", fmt.Errorf("Panic") // неверный формат
+		panic("Panic: Invalid format") // неверный формат
 	}
 
 	a, errA := strconv.Atoi(p[0])
-	b, errB := strconv.Atoi(p[2])     // Первый и третий элементы массива  преобразуются из строкового представления чисел в целочисленное
-	isR := errA != nil && errB != nil // Проверяется, являются ли errA и errB не nil. Если оба равны nil, значит, оба числа a и b являются арабскими числами. В противном случае, хотя бы одно из чисел является римским.
+	b, errB := strconv.Atoi(p[2]) // Первый и третий элементы массива  преобразуются из строкового представления чисел в целочисленное
+	isR := errA != nil && errB != nil
 
 	if isR {
-		a, errA = rotoint(p[0]) // rotoint преобразует римские числа в арабские. Если преобразование невозможно или результат отрицательный, возвращается ошибка.
-		b, errB = rotoint(p[2])
-	}
-
-	if errA != nil || errB != nil { // Проверяется, есть ли ошибки в преобразовании чисел. Если хотя бы одно число не удалось преобразовать, возвращается ошибка
-		return "", fmt.Errorf("Panic") // неверный формат чисел
-	}
-
-	res, err := calculate(a, b, p[1])
-	if err != nil {
-		return "", err
-	}
-
-	if isR {
-		romanRes := intor(res)
-		if romanRes == "" {
-			return "", fmt.Errorf("Panic")
+		a = rotoint(p[0]) // rotoint преобразует римские числа в арабские. Если преобразование невозможно или результат отрицательный, возвращается ошибка.
+		b = rotoint(p[2])
+	} else {
+		if errA != nil || errB != nil {
+			panic("Panic: Invalid numbers")
 		}
-		return romanRes, nil
+
+		if (a < 1 || a > 10) || (b < 1 || b > 10) {
+			panic("Panic: Numbers must be between 1 and 10")
+		}
 	}
 
-	return strconv.Itoa(res), nil
+	res := calculate(a, b, p[1])
+
+	if isR {
+		if res <= 0 {
+			panic("Panic: Result cannot be negative or zero in Roman numerals")
+		}
+		return intor(res)
+	}
+
+	return strconv.Itoa(res)
 }
 
 func main() {
@@ -117,14 +117,17 @@ func main() {
 	fmt.Println("Введите выражение (например, '2 + 3' или 'II * III'): ")
 	for scanner.Scan() {
 		input := scanner.Text()
-		res, err := proinp(input)
-		if err != nil {
-			fmt.Println(err)
-			return // Прекращаем выполнение при ошибке
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Println(r)
+				}
+			}()
+			res := proinp(input)
+			fmt.Println(res)
+		}()
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
 		}
-		fmt.Println("Результат:", res)
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
 	}
 }
