4746c01c0ba16c9afaad88ef3d0bbda629674d99
Madpeacher <rainbownatural@yandex.ru>
2024-07-05 01:15:53 +0300

    Update aa.go

--- aa.go
+++ aa.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -22,10 +23,24 @@ var artor = []struct {
 	{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"},
 }
 
+// Регулярное выражение для проверки корректности римских цифр
+var validRomanPattern = regexp.MustCompile(`^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$`)
+
+// Проверка, является ли строка корректным римским числом
+func isValidRoman(s string) bool {
+	return validRomanPattern.MatchString(s)
+}
+
 func rotoint(s string) int {
+	// Проверка на корректность римского числа
+	if !isValidRoman(s) {
+		panic("Panic: Invalid Roman numeral")
+	}
+
 	n := len(s)
 	res := 0
 	for i := 0; i < n; {
+		// Проверка на двухсимвольные комбинации
 		if i+1 < n {
 			if value, ok := rtoar[s[i:i+2]]; ok {
 				res += value
@@ -33,14 +48,7 @@ func rotoint(s string) int {
 				continue
 			}
 		}
-
-		// Check for invalid Roman numeral pattern
-		if i+3 <= n {
-			if _, ok := rtoar[s[i:i+4]]; ok {
-				panic("Panic: Invalid Roman numeral")
-			}
-		}
-
+		// Проверка на односивольные символы
 		if value, ok := rtoar[string(s[i])]; ok {
 			res += value
 			i++
@@ -51,7 +59,12 @@ func rotoint(s string) int {
 	return res
 }
 
+// Преобразование арабского числа в римское
 func intor(num int) string {
+	// Проверка на недопустимые значения
+	if num <= 0 {
+		panic("Panic: Result cannot be negative or zero in Roman numerals")
+	}
 	var res strings.Builder
 	for _, entry := range artor {
 		for num >= entry.value {
@@ -61,7 +74,6 @@ func intor(num int) string {
 	}
 	return res.String()
 }
-
 func calculate(a, b int, op string) int {
 	switch op {
 	case "+":
@@ -71,6 +83,7 @@ func calculate(a, b int, op string) int {
 	case "*":
 		return a * b
 	case "/":
+		// Проверка деления на ноль
 		if b == 0 {
 			panic("Panic: Division by zero")
 		}
@@ -80,33 +93,35 @@ func calculate(a, b int, op string) int {
 	}
 }
 
+// Обработка ввода и выполнение вычислений
 func proinp(input string) string {
 	p := strings.Fields(input)
 	if len(p) != 3 {
 		panic("Panic: Invalid format")
 	}
 
+	// Проверка, является ли ввод арабскими или римскими числами
 	a, errA := strconv.Atoi(p[0])
 	b, errB := strconv.Atoi(p[2])
 	isR := errA != nil && errB != nil
 
+	// Преобразование римских чисел в арабские
 	if isR {
 		a = rotoint(p[0])
 		b = rotoint(p[2])
 	}
 
+	// Проверка на допустимый диапазон значений
 	if (a < 1 || a > 10) || (b < 1 || b > 10) {
 		panic("Panic: Numbers must be between 1 and 10")
 	}
 
+	// Выполнение арифметической операции
 	res := calculate(a, b, p[1])
 
+	// Преобразование результата в римское число при необходимости
 	if isR {
-		romanRes := intor(res)
-		if romanRes == "" {
-			panic("Panic: Result cannot be negative in Roman numerals")
-		}
-		return romanRes
+		return intor(res)
 	}
 
 	return strconv.Itoa(res)
